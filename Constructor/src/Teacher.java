864f7d925ef79b86a108c9a4aeae0275aacce7a3
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2023-08-30 21:18:00 +0600

    Add files via upload

---
+++ Constructor/src/Teacher.java
@@ -0,0 +1,19 @@
+public class Teacher {
+    String name;
+
+    Teacher(String nm){
+
+        name = nm ;
+
+    }
+
+    void  displayInfo(){
+
+        System.out.println("Name : "+name);
+
+    }
+
+
+
+}
+
---
+++ Constructor/src/Teacher_info.java
@@ -0,0 +1,6 @@
+public class Teacher_info {
+    public static void main(String[] args) {
+        Teacher Teacher1 = new Teacher("Fahim");
+        Teacher1.displayInfo();
+    }
+}
